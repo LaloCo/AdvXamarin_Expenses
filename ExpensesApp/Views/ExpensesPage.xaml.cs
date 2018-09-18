24a030936819e45f6b60743bc88218943d05deaf
LaloCo <lalorosas@outlook.com>
2018-09-18 06:56:35 -0500

    Initial check-in of module ExpensesApp

---
+++ ExpensesApp/Views/ExpensesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ExpensesApp.Views
+{
+    public partial class ExpensesPage : ContentPage
+    {
+        public ExpensesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
