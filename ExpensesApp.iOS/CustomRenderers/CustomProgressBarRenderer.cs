7206321bb4a034a28ccc36bafd40996a85b726df
LaloCo <lalorosas@outlook.com>
2018-09-20 06:02:47 -0500

    AdvXamarin-0200-CustomRendereriOS

---
+++ ExpensesApp.iOS/CustomRenderers/CustomProgressBarRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CoreGraphics;
+using ExpensesApp.iOS.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ProgressBar), typeof(CustomProgressBarRenderer))]
+namespace ExpensesApp.iOS.CustomRenderers
+{
+    public class CustomProgressBarRenderer : ProgressBarRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ProgressBar> e)
+        {
+            base.OnElementChanged(e);
+
+            LayoutSubviews();
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            float x = 1.0f;
+            float y = 4.0f;
+
+            CGAffineTransform transform = CGAffineTransform.MakeScale(x, y);
+            Transform = transform;
+        }
+    }
+}
