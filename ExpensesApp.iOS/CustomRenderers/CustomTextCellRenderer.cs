9e19407f6f8a84408934c1e1d284c53280efb07a
LaloCo <lalorosas@outlook.com>
2018-09-21 05:30:24 -0500

    AdvXamarin-0240-CustomTextCells

---
+++ ExpensesApp.iOS/CustomRenderers/CustomTextCellRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ExpensesApp.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TextCell), typeof(CustomTextCellRenderer))]
+namespace ExpensesApp.iOS.CustomRenderers
+{
+    public class CustomTextCellRenderer : TextCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            switch(item.StyleId)
+            {
+                case "none":
+                    cell.Accessory = UITableViewCellAccessory.None;
+                    break;
+                case "checkmark":
+                    cell.Accessory = UITableViewCellAccessory.Checkmark;
+                    break;
+                case "detail-button":
+                    cell.Accessory = UITableViewCellAccessory.DetailButton;
+                    break;
+                case "detail-disclosure-button":
+                    cell.Accessory = UITableViewCellAccessory.DetailDisclosureButton;
+                    break;
+                case "disclosure":
+                default:
+                    cell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+                    break;
+            }
+
+            return cell;
+        }
+    }
+}
