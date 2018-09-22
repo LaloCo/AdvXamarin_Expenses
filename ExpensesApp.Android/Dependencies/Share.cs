046f8c0b9e7b25aa028f85c0005b690d9fa411d0
LaloCo <lalorosas@outlook.com>
2018-09-22 07:02:05 -0500

    AdvXamarin-0380-SharingAndroid

--- ExpensesApp.Android/Dependencies/Share.cs
+++ ExpensesApp.Android/Dependencies/Share.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Android.Content;
+using Android.Support.V4.Content;
 using ExpensesApp.Droid.Dependencies;
 using ExpensesApp.Interfaces;
 using Xamarin.Forms;
@@ -9,9 +11,21 @@ namespace ExpensesApp.Droid.Dependencies
 {
     public class Share : IShare
     {
-        public async Task Show(string title, string message, string filePath)
+        public Task Show(string title, string message, string filePath)
         {
+            var intent = new Intent(Intent.ActionSend);
+            intent.SetType("text/plain");
+            var documentUri = FileProvider.GetUriForFile(Forms.Context.ApplicationContext, "com.lalorosas.ExpensesApp.provider", new Java.IO.File(filePath));
 
+            intent.PutExtra(Intent.ExtraStream, documentUri);
+            intent.PutExtra(Intent.ExtraText, title);
+            intent.PutExtra(Intent.ExtraSubject, message);
+
+            var chooserIntent = Intent.CreateChooser(intent, title);
+            chooserIntent.SetFlags(ActivityFlags.GrantReadUriPermission);
+            Android.App.Application.Context.StartActivity(chooserIntent);
+
+            return Task.FromResult(true);
         }
     }
 }
