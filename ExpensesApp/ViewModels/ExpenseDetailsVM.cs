4e353f40c8df8f869dd1177934ebba0591056086
LaloCo <lalorosas@outlook.com>
2018-09-24 06:58:29 -0500

    AdvXamarin-0400-Behaviors

---
+++ ExpensesApp/ViewModels/ExpenseDetailsVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using ExpensesApp.Models;
+
+namespace ExpensesApp.ViewModels
+{
+    public class ExpenseDetailsVM : INotifyPropertyChanged
+    {
+        private Expense expense;
+        public Expense Expense
+        {
+            get { return expense; }
+            set
+            {
+                expense = value;
+                OnPropertyChanged("Expense");
+            }
+        }
+
+        public ExpenseDetailsVM()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
