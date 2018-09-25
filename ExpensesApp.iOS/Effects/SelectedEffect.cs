8b00f8d663d144ea8248334794c23f6465e1622f
LaloCo <lalorosas@outlook.com>
2018-09-25 07:40:21 -0500

    AdvXamarin-0460-ChangeBGiOS

--- ExpensesApp.iOS/Effects/SelectedEffect.cs
+++ ExpensesApp.iOS/Effects/SelectedEffect.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
 using ExpensesApp.iOS.Effects;
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -9,13 +11,28 @@ namespace ExpensesApp.iOS.Effects
 {
     public class SelectedEffect : PlatformEffect
     {
-        public SelectedEffect()
-        {
-        }
+        UIColor selectedColor;
 
         protected override void OnAttached()
         {
+            selectedColor = new UIColor(176 / 255, 152 / 255, 164 / 255, 255 / 255);
+        }
 
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            if(args.PropertyName == "IsFocused")
+            {
+                if(Control.BackgroundColor != selectedColor)
+                {
+                    Control.BackgroundColor = selectedColor;
+                }
+                else
+                {
+                    Control.BackgroundColor = UIColor.White;
+                }
+            }
         }
 
         protected override void OnDetached()
