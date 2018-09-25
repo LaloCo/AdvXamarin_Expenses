731cf4f671d8b13c9b3c23861de8d3dca19631de
LaloCo <lalorosas@outlook.com>
2018-09-25 06:05:40 -0500

    AdvXamarin-0420-Effects

---
+++ ExpensesApp.Android/Effects/SelectedEffect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ExpensesApp.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("LPA")]
+[assembly: ExportEffect(typeof(SelectedEffect), "SelectedEffect")]
+namespace ExpensesApp.Droid.Effects
+{
+    public class SelectedEffect : PlatformEffect
+    {
+        public SelectedEffect()
+        {
+        }
+
+        protected override void OnAttached()
+        {
+
+        }
+
+        protected override void OnDetached()
+        {
+
+        }
+    }
+}
---
+++ ExpensesApp.iOS/Effects/SelectedEffect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ExpensesApp.iOS.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("LPA")]
+[assembly: ExportEffect(typeof(SelectedEffect), "SelectedEffect")]
+namespace ExpensesApp.iOS.Effects
+{
+    public class SelectedEffect : PlatformEffect
+    {
+        public SelectedEffect()
+        {
+        }
+
+        protected override void OnAttached()
+        {
+
+        }
+
+        protected override void OnDetached()
+        {
+
+        }
+    }
+}
