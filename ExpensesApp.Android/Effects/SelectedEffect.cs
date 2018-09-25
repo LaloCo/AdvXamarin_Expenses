590cbc9fd477eb7dbc83691b55a9a2fb704f535d
LaloCo <lalorosas@outlook.com>
2018-09-25 06:59:33 -0500

    AdvXamarin-0440-ChangeBGAndroid

--- ExpensesApp.Android/Effects/SelectedEffect.cs
+++ ExpensesApp.Android/Effects/SelectedEffect.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using Android.Graphics.Drawables;
 using ExpensesApp.Droid.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
@@ -9,13 +11,35 @@ namespace ExpensesApp.Droid.Effects
 {
     public class SelectedEffect : PlatformEffect
     {
-        public SelectedEffect()
-        {
-        }
+        Android.Graphics.Color selectedColor;
 
         protected override void OnAttached()
         {
+            selectedColor = new Android.Graphics.Color(176, 152, 164);
+        }
 
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (((ColorDrawable)Control.Background).Color != selectedColor)
+                    {
+                        Control.SetBackgroundColor(selectedColor);
+                    }
+                    else
+                    {
+                        Control.SetBackgroundColor(Android.Graphics.Color.White);
+                    }
+                }
+            }
+            catch(InvalidCastException)
+            {
+                Control.SetBackgroundColor(selectedColor);
+            }
         }
 
         protected override void OnDetached()
